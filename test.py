f7f206f9c8a4c1fb72e100b94b91a295a21f3b06
yinscott <33796214+yinscott@users.noreply.github.com>
2017-11-30 14:45:49 +0800

    Update test.py

--- test.py
+++ test.py
@@ -2,3 +2,9 @@ import multiprocessing
 import six
 
 import tensorflow as tf
+
+CSV_COLUMNS = ['age', 'workclass', 'fnlwgt', 'education', 'education_num',
+               'marital_status', 'occupation', 'relationship', 'race', 'gender',
+               'capital_gain', 'capital_loss', 'hours_per_week',
+               'native_country', 'income_bracket']
+
